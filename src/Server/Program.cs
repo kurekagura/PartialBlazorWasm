5f14ddbb8b51021af57b22ead669690ad82f9931
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-11-05 00:43:17 +0900

    発行プロファイル作成。

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.FileProviders;
 
 namespace WebApp
 {
@@ -27,6 +28,15 @@ namespace WebApp
 
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
+
+            //自分で物理フォルダとURLを指定。
+            var contentRootPath = app.Environment.ContentRootPath;  //プロジェクトのルート
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                ServeUnknownFileTypes = true,
+                FileProvider = new PhysicalFileProvider(Path.Combine(contentRootPath, "Pages/AI/wwwroot")),
+                RequestPath = "/AI",
+            });
 
             app.UseRouting();
 
